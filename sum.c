e5d22db94b2821079494adb522db2ac453437a2f
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2024-02-15 20:50:02 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,sum;
+    printf("Enter the value of a");
+    scanf("%d",&a);
+    
+    printf("Enter the value of b");
+    scanf("%d",&b);
+    
+    printf("Enter the value of c");
+    scanf("%d",&c);
+    
+    sum=a+b+c;
+    printf("the sum of three numbers is:%d",sum);
+    
+    return 0;
+           }
