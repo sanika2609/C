3d8df77d6ec7a2d1b0a819c3c2e4e74774ba66ec
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2024-02-16 22:44:46 +0530

    Add files via upload

---
+++ sum,difference.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+	int m=10,n=4,sum,difference;
+    float a=4.0,b=2.0,s,dif;
+    
+    
+    
+    sum=m+n;
+    printf("%d\t",sum);
+    difference=m-n;
+    printf("%d\n",difference);
+    
+    s=a+b;
+    printf("%f\t",s);
+    dif=a-b;
+    printf("%f",dif);
+
+    return 0;
+}
