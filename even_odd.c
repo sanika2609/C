09576e9f566dec9fa53b7b20c6d6c245b0832a88
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2024-02-17 23:57:49 +0530

    Add files via upload

---
+++ even_odd.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int num,even,odd;
+    printf("Enter the number");
+    scanf("%d",&num);
+    
+     if(num%2==0)
+     {
+                printf("num is Even");
+                }
+                else
+                {
+                    printf("num is odd");
+                    }
+                    return 0;
+                    }
+                    
