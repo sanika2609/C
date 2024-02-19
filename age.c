f78ed67c67b57ce049594175070a5ec3dc60d98a
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2024-02-19 20:30:57 +0530

    Add files via upload

---
+++ age.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int age;
+    if(age>=18)
+    {
+               printf("The person can vote);
+               }
+               else
+               {
+                   printf("The person can't vote);
+                   }
+                   return 0:
+                          }
