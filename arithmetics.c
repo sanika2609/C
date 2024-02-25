f8bc3fa44de6964414a94d34be6e07f92d81d4da
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2024-02-25 14:37:49 +0530

    Add files via upload

---
+++ arithmetics.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,add,sub,multi,div;
+    printf("Enter a");
+    scanf("%d",&a);
+     printf("Enter b");
+    scanf("%d",&b);
+    add=a+b;
+    printf("The addition of two numbers is:%d\n",add);
+    sub=a-b;
+     printf("The subtraction of two numbers is:%d\n",sub);
+     multi=a*b;
+     printf("The multiplication of two numbers is:%d\n",multi);
+    div=a/b;
+     printf("The division of two numbers is:%d\n",div);
+    
+}
