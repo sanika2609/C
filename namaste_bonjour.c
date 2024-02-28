e5e95be940f5327af502730bd51c21ab3657d798
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2024-02-28 21:10:30 +0530

    Add files via upload

---
+++ namaste_bonjour.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void Namaste();
+void Bonjour();
+
+int main(){
+    char ch;
+  printf("Enter F for french and I for indian");
+  
+  scanf("%c",&ch);
+  if(char==I)
+  {
+             Namaste();
+             }
+             else
+             {
+                 Bonjour();
+                 }  
+    
+    return 0;
+}
+void Namaste();
+{
+     printf("Namaste\n");
+     }
+     void Bonjour();
+     {
+          printf("Bonjour");
+          }
