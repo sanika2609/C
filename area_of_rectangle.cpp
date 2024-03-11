aed037768889706984949a0d1c81f7297db46c78
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2024-03-11 19:30:00 +0530

    Add files via upload

---
+++ area_of_rectangle.cpp
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+int main() {
+       float l,b,area;
+       printf("Enter the length of rectangle");
+       scanf("%f",&l);
+       printf("Enter the breadth");
+       scanf("%f",&b);
+       area=l*b;
+       printf("The area of rectangle is:%f",area);
+
+    return 0;
+} 
