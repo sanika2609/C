fd257bcfa0c69a18a9e92732b649c78761c327b4
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2024-03-20 20:32:57 +0530

    Add files via upload

---
+++ ar_of_rec_fun,c.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void area() {
+    int length;
+   int breadth;
+}
+int main(){
+    int area,length=5,breadth=6;
+    
+    int r=(length*breadth);
+    printf("Area of the rectangle is:%d",r);
+
+    
+       return 0;
+}
