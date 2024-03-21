0f28ab36d5c12d2693bf32c0cb4b76681f9187ad
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2024-03-21 20:02:21 +0530

    Add files via upload

---
+++ area_of_square_fun.c.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void area() {
+    int side;
+}
+int main()
+{
+    int area,side;
+    printf("Enter the side of square");
+    scanf("%d",&side);
+    area=side*side;
+    printf("The area of square is :%d",area);
+    
+    return 0;
+
+}
