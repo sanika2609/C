996a98d7217e620c393abb4b2ca59ecc2507ab5b
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2024-03-31 19:50:20 +0530

    Add files via upload

---
+++ alphabets.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Alphabets from A - Z are: \n");
+
+    
+    for(int i=65; i<=90; i++)
+    {
+      
+        printf("%c ", i);
+    }
+    printf("\n");
+    return 0;
+}
