0cbf1349908bfb79f43fd443185b900919c78323
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2024-04-12 19:55:07 +0530

    Add files via upload

---
+++ arr_sum.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    int i,arr[]={1,2,3,4,5,6,7,8,9,10};
+    int sum=arr[0]+arr[1]+arr[2]+arr[3]+arr[4]+arr[5]+arr[6]+arr[7]+arr[8]+arr[9];
+    printf("The sum of all array elements is:%d",sum);
+    return 0;
+}
