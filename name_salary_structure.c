2d0533c863e6b14f1b607e7ec25056328b3c6313
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2024-05-19 20:26:05 +0530

    Create name_salary_structure.c

---
+++ name_salary_structure.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+struct Person{
+char name[50];
+float salary;
+}
+Person1;
+int main(){
+strcpy(person1.name,"sanika pawar");
+person1.salary=100000;
+printf("name:%s\n",person1.name);
+printf("salary:%f\n",person1.salary);
+return 0;
+}
