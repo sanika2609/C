b442a48b3d1eee20cbeb674a1b545509d481c8d1
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2024-06-10 20:36:16 +0530

    Update typedef.c

--- typedef.c
+++ typedef.c
@@ -17,8 +17,8 @@ typedef struct
                     student s1={p1,31,50.5};
                     
                     printf("name of the student is:%s\n",s1.p.name);
-                    printf("age of the student is:%s\n",s1.p.age);
-                    printf("Roll_no of student is:%s\n",s1.Roll_no);
+                    printf("age of the student is:%d\n",s1.p.age);
+                    printf("Roll_no of student is:%d\n",s1.Roll_no);
                     printf("marks of the student is:%f\n",s1.marks);
                     return;
                     }
