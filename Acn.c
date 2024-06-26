a2ad04072de167207fb46e656cf01fd1f1416911
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2024-06-26 21:55:02 +0530

    Create Acn.c

---
+++ Acn.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define BUFSIZE 1024
+
+int main() {
+    int sockfd;
+    char buffer[BUFSIZE];
+    char *message = "Hello, Server!";
+    struct sockaddr_in servaddr;
+
+    // Creating socket file descriptor
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&servaddr, 0, sizeof(servaddr));
+
+    // Filling server information
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(PORT);
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+
+    int n, len;
+
+    sendto(sockfd, (const char *)message, strlen(message), MSG_CONFIRM, (const struct sockaddr *)&servaddr, sizeof(servaddr));
+    printf("Message sent to server\n");
+
+    close(sockfd);
+    return 0;
+}
